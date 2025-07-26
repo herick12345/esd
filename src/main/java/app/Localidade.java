d8a254a3f33721e0bf2d58e69a764480f3387e77
herick12345 <herickmtm@gmail.com>
2025-07-26 13:20:20 -0300

    Delete src directory

--- src/main/java/app/Localidade.java
+++
@@ -1,7 +0,0 @@
-package app;
-
-// use esta classe para representar uma localidade retornada como resultado
-// Lembre que um "record" é uma classe especial que já fornece construtor e getters
-public record Localidade (String pais, String local) {
-    // você pode acrescentar métodos para implementar novas operações (se necessárias !)
-}
